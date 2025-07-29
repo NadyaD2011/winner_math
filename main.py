6ca4c68d45db17a5b94f88a52af538326f36973b
NadyaEvp <t_evp@mail.ru>
2025-07-29 19:22:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,33 +6,33 @@ def simulate_matches(teams, num_simulations=1000):
     results = {team: 0 for team in teams}
     matches = list(itertools.combinations(teams.keys(), 2))
 
-    for team1, team2 in matches:
-        wins_team1, wins_team2 = 0, 0
+    for team_first, team_second in matches:
+        wins_team_first, wins_team_second = 0, 0
 
         for _ in range(num_simulations):
-            p_team1 = teams[team1] / (teams[team1] + teams[team2])
-            if random.random() < p_team1:
-                wins_team1 += 1
+            p_team_first = teams[team_first] / (teams[team_first] + teams[team_second])
+            if random.random() < p_team_first:
+                wins_team_first += 1
             else:
-                wins_team2 += 1
+                wins_team_second += 1
 
-        if wins_team1 > wins_team2:
-            results[team1] += 3
-        elif wins_team2 > wins_team1:
-            results[team2] += 3
+        if wins_team_first > wins_team_second:
+            results[team_first] += 3
+        elif wins_team_second > wins_team_first:
+            results[team_second] += 3
         else:
-            results[team1] += 1
-            results[team2] += 1
+            results[team_first] += 1
+            results[team_second] += 1
 
     sorted_results = dict(sorted(results.items(), key=lambda x: x[1], reverse=True))
     return {team: rank for rank, team in enumerate(sorted_results, 1)}
 
 
-def play_match(team1, team2, ratings):
-    if ratings[team1] / (ratings[team1] + ratings[team2]) > random.random():
-        return team1
+def play_match(team_first, team_second, ratings):
+    if ratings[team_first] / (ratings[team_first] + ratings[team_second]) > random.random():
+        return team_first
     else:
-        return team2
+        return team_second
 
 
 def simulate_tournament(groups, num_simulations=10000):
@@ -44,13 +44,13 @@ def simulate_tournament(groups, num_simulations=10000):
     for _ in range(num_simulations):
         group_results = {group_name: simulate_matches(group_teams) for group_name, group_teams in groups.items()}
 
-        first_semi_finalist = list(group_results['gruppa_d'].keys())[list(group_results['gruppa_d'].values()).index(1)]
-        second_semi_finalist = list(group_results['gruppa_e'].keys())[list(group_results['gruppa_e'].values()).index(2)]
-        third_semi_finalist = list(group_results['gruppa_d'].keys())[list(group_results['gruppa_d'].values()).index(2)]
-        fourth_semi_finalist = list(group_results['gruppa_e'].keys())[list(group_results['gruppa_e'].values()).index(1)]
+        first_semi_finalist = list(group_results['group_d'].keys())[list(group_results['group_d'].values()).index(1)]
+        second_semi_finalist = list(group_results['group_e'].keys())[list(group_results['group_e'].values()).index(2)]
+        third_semi_finalist = list(group_results['group_d'].keys())[list(group_results['group_d'].values()).index(2)]
+        fourth_semi_finalist = list(group_results['group_e'].keys())[list(group_results['group_e'].values()).index(1)]
 
-        first_semi_final = {first_semi_finalist: groups['gruppa_d'][first_semi_finalist], second_semi_finalist: groups['gruppa_e'][second_semi_finalist]}
-        second_semi_final = {third_semi_finalist: groups['gruppa_d'][third_semi_finalist], fourth_semi_finalist: groups['gruppa_e'][fourth_semi_finalist]}
+        first_semi_final = {first_semi_finalist: groups['group_d'][first_semi_finalist], second_semi_finalist: groups['group_e'][second_semi_finalist]}
+        second_semi_final = {third_semi_finalist: groups['group_d'][third_semi_finalist], fourth_semi_finalist: groups['group_e'][fourth_semi_finalist]}
 
         finalists = [
             play_match(*first_semi_final.keys(), first_semi_final),
@@ -59,9 +59,9 @@ def simulate_tournament(groups, num_simulations=10000):
 
         third_place_contenders = [team for team in list(first_semi_final.keys()) + list(second_semi_final.keys()) if team not in finalists]
 
-        final_winner = play_match(finalists[0], finalists[1], {**groups['gruppa_d'], **groups['gruppa_e']})
+        final_winner = play_match(finalists[0], finalists[1], {**groups['group_d'], **groups['group_e']})
         second_place = [team for team in finalists if team != final_winner][0]
-        third_place_winner = play_match(third_place_contenders[0], third_place_contenders[1], {**groups['gruppa_d'], **groups['gruppa_e']})
+        third_place_winner = play_match(third_place_contenders[0], third_place_contenders[1], {**groups['group_d'], **groups['group_e']})
 
         winners[final_winner] += 1
         second_places[second_place] += 1
@@ -85,9 +85,9 @@ def calculate_probabilities(winners, second_places, third_places, num_simulation
 
 
 def main():
-    gruppa_d = {'Франция': 90, 'Дания': 85, 'Австралия': 75, 'Тунис': 70}
-    gruppa_e = {'Германия': 0.85, 'Япония': 0.65, 'Испания': 0.85, 'Коста-Рика': 0.55}
-    groups = {'gruppa_d': gruppa_d, 'gruppa_e': gruppa_e}
+    group_d = {'Франция': 90, 'Дания': 85, 'Австралия': 75, 'Тунис': 70}
+    group_e = {'Германия': 0.85, 'Япония': 0.65, 'Испания': 0.85, 'Коста-Рика': 0.55}
+    groups = {'group_d': group_d, 'group_e': group_e}
 
     num_simulations = 1000
     winners, second_places, third_places = simulate_tournament(groups, num_simulations)
